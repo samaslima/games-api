76b807cbac1778b4d738ae9489ab2dc139afa343
samaslima <samaslimag@gmail.com>
2024-06-05 13:52:51 -0300

    add database

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
 const gameModel = require('./models/game');
+const routerGame = require('./routes/game-routes');
+
+app.use(express.json());
+app.use('/games', routerGame);
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
