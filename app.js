11e11e5def08df425e83a2bbf9b24d60ef138035
samaslima <samaslimag@gmail.com>
2024-06-10 15:16:19 -0300

    update app

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const gameModel = require('./models/game');
 const playerModel = require('./models/player');
 
 const routerGame = require('./routes/game-routes');
+const routerPlayer = require('./routes/player-routes');
 
 app.use(express.json());
 app.use('/games', routerGame);
+app.use('/players', routerPlayer);
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
