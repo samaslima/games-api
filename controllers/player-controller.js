730533e78678fc10f31fa4c26e0b9263ea93fdf2
samaslima <samaslimag@gmail.com>
2024-06-10 15:13:43 -0300

    add player controller

---
+++ controllers/player-controller.js
@@ -0,0 +1,34 @@
+const playerModel = require('./../models/player');
+
+const createPlayer = async (req, res) => {
+    const player = await playerModel.create(req.body);
+    return res.status(200).json(player);
+} 
+
+const findById = async (req, res) => {
+    const player = await playerModel.findOne({ where: { id: req.params.id }});
+    return res.status(200).json(player);
+}
+
+const findAll = async (req, res) => {
+    const games = await playerModel.findAll();
+    return res.status(200).json(games);
+}
+
+const deletePlayer = async (req, res) => {
+    await playerModel.destroy({ where: { id: req.params.id }});
+    return res.status(200).json({ msg: `player with id ${ req.params.id } deleted`});
+}
+
+const updatePlayer = async (req, res) => {
+    await playerModel.update(req.body, { where: { id: req.params.id } });
+    return res.status(200).json({ msg: `player with id ${ req.params.id } updated`});
+}
+
+module.exports = {
+    createPlayer,
+    findById,
+    findAll,
+    deletePlayer,
+    updatePlayer
+}
